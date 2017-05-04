9db16a510b1f69252ac452ff8240d31b18b3c828
Liam Reilly <liam.reilly@zuto.com>
2017-05-04 11:40:26 +0100

    Added basic calculator with Add feature + test

---
+++ SpecFlow.AcceptanceTests/SpecFlow.AcceptanceTests/Calculator/Add/Add.steps.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.AcceptanceTests.Calculator.Add
+{
+    [Binding]
+    public sealed class AddSteps
+    {
+        internal SpecFlow.Calculator.Calculator Calculator;
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {
+            Calculator = new SpecFlow.Calculator.Calculator
+            {
+                FirstNumber = number
+            };
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            Calculator.SecondNumber = number;
+        }
+
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            Calculator.Add();
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.That(Calculator.Result, Is.EqualTo(result));
+        }
+    }
+}
---
+++ SpecFlow/SpecFlow/Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SpecFlow.Calculator
+{
+    [SuppressMessage("ReSharper", "MethodNameNotMeaningful")]
+    public class Calculator
+    {
+        public long FirstNumber { get; set; }
+        public long SecondNumber { get; set; }
+        public long Result { get; private set; }
+        
+        public long Add() => Result = FirstNumber + SecondNumber;
+    }
+}
